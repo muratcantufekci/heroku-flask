10f3717df11e6f92da0a7ff62b189d0ddd056b4c
Muratcan Tüfekçi <1600002065@STU.IKU.EDU.TR>
2021-04-28 00:07:30 +0300

    initialize

---
+++ app/app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+from flask import Flask
+from flask import request,jsonify, render_template
+import pickle
+app = Flask(__name__)
+
+
+model = pickle.load(open('diagnosismodel.pkl', 'rb'))
+@app.route('/')
+def home():
+    
+    return '<h1> API server is working </h1>'
+
+@app.route('/predict')
+def predict():
+   
+    diagnosis_predict = model.predict([[15,35,36,12,34,28,23,17,18,18,15]]) 
+    return str(diagnosis_predict)
+
+
+app.run()
+    
+
+
+# In[ ]:
+
+
+
+
