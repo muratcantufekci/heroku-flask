8d08a705cf0ad8afed4b1e4bf65f2310ac8b5883
Muratcan Tüfekçi <1600002065@STU.IKU.EDU.TR>
2021-04-29 03:04:44 +0300

    fix

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def predict():
                                        request.args['Symptom_9'],
                                        request.args['Symptom_10'],
                                        request.args['Symptom_11']]]) 
-    return (index.html)
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
